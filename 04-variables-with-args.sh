449e4fb46f483e19abb02e0f8ea7fda644569d97
Ayesha086 <ayeshacs086@gmail.com>
2025-04-28 00:56:24 +0530

    conversation added

--- 04-variables-with-args.sh
+++ 04-variables-with-args.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Rahim # no space after and before equal
-PERSON2=Robert
+PERSON1=$1 # no space after and before equal
+PERSON2=$2
 
 echo "$PERSON1:: Hi $PERSON2"
 echo "$PERSON2:: Hello$PERSON1"
