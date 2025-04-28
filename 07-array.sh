ff64a217c68174da8de335b7a3b907787598d8e6
Ayesha086 <ayeshacs086@gmail.com>
2025-04-28 22:21:13 +0530

    conversation added

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("pushpa""rrr""devara")
+#index starts from 0,size is 3
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie:${MOVIES[1]}"
+echo "Third movie:${MOVIES[2]}"
+
+ech0 "All movies are: ${MOVIES[@]}"
