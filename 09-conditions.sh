27ac00f90b4b0598b64c28ba381485b97a88338d
Ayesha086 <ayeshacs086@gmail.com>
2025-04-29 00:01:52 +0530

    conversation added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,7 +5,7 @@ NUMBER=$1
 # -gt, -lt, -eq, -ge, -le
 if [ $NUMBER -gt 100 ]
 then
-    echo "Given number is greatee then 100"
+    echo "Given number is greater then 100"
 else
     echo "Given number is less than 0r equal to 100"
 fi
